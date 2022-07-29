197dfd15dc3eb0d5840abca3874e7c06a170e18f
mCassy <misha.krasilynets@gmail.com>
2022-07-29 09:01:04 +0300

    Add files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,15 +3,15 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup() {
-        return this.authService.signup();
-    }
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
 
-    @Post('signin')
-    signin() {
-        return this.authService.signin();
-    }
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
 }
